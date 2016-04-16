fd2c6d679dbefe2067c5308684a4350f1f652cb2
Hernán Herrera <h.herrera@me.com>
2016-04-16 00:24:10 -0300

    Initial Commit

---
+++ PracticoMovil/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PracticoMovil
+//
+//  Created by Hernán Herrera on 16-04-16.
+//  Copyright © 2016 Endline Ltda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
